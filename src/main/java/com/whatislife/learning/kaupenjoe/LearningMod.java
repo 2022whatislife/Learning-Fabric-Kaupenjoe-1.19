6b2970c86e3403bda132a356c5e1518f2ec94750
2022whatislife <2022whatislife@gmail.com>
2022-07-16 01:50:31 -0400

    added learning_ingot

--- src/main/java/com/whatislife/learning/kaupenjoe/LearningMod.java
+++ src/main/java/com/whatislife/learning/kaupenjoe/LearningMod.java
@@ -1,14 +1,16 @@
 package com.whatislife.learning.kaupenjoe;
 
+import com.whatislife.learning.kaupenjoe.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LearningMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("learning-kaupenjoe");
+	public static final String MOD_ID = "learning-kaupenjoe";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
